ee8cc9b684a3b434808fef49d45ad2dff993f11f
Andrew Nguyen <andy.andino93@gmail.com>
2017-10-25 11:42:44 -0700

    finished comp repo

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// TODO
+// const GroceryListItem = ['eggs', 'bacon'];
+const App = () => (
+	<div>
+		<h2> Grocery list </h2>
+		<GroceryList list={['eggs', 'cheese']}/>
+	</div>
+);
+
+const GroceryList = (props) => (
+	<ul> 
+		{props.list.map(list => 
+			<GroceryListItem list={list} />
+		)}
+	</ul>
+);
+
+class GroceryListItem extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			done: false
+		};
+	}
+
+	onListItemClick() {
+		this.setState({
+			done: !this.state.done
+		});
+	}
+
+	render() {
+		let style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+		return (
+			<li style={style} onClick={this.onListItemClick.bind(this)}> {this.props.list} </li>
+		);
+	}
+
+}
+
+
+ReactDOM.render(<App />, document.getElementById("app"))
+
